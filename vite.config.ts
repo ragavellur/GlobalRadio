d212919dac9ac1a8e797b0771145134e401190ab
ragavellur <raga.vellur@gmail.com>
2026-07-21 17:00:04 +0530

    replace PWA icons with trademark SVG design (Indian tricolor + play button)

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['icon-192.png', 'icon-512.png'],
+      includeAssets: ['icon-48.png', 'icon-72.png', 'icon-96.png', 'icon-128.png', 'icon-144.png', 'icon-152.png', 'icon-192.png', 'icon-384.png', 'icon-512.png', 'favicon.png'],
       manifest: {
         name: 'Global Radio Explorer',
         short_name: 'Global Radio',
@@ -21,7 +21,14 @@ export default defineConfig({
         scope: '/',
         start_url: '/',
         icons: [
+          { src: '/icon-48.png', sizes: '48x48', type: 'image/png' },
+          { src: '/icon-72.png', sizes: '72x72', type: 'image/png' },
+          { src: '/icon-96.png', sizes: '96x96', type: 'image/png' },
+          { src: '/icon-128.png', sizes: '128x128', type: 'image/png' },
+          { src: '/icon-144.png', sizes: '144x144', type: 'image/png' },
+          { src: '/icon-152.png', sizes: '152x152', type: 'image/png' },
           { src: '/icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/icon-384.png', sizes: '384x384', type: 'image/png' },
           { src: '/icon-512.png', sizes: '512x512', type: 'image/png' },
           { src: '/icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' },
         ],
